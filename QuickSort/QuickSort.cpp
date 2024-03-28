e2ffc60b8e91e63338a6c6c3d7be79ec0e358063
tacosz <nabilnasruddin.offc@gmail.com>
2024-03-28 20:30:54 +0700

    penambahan while & step 5,6,7,8,9,10

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -42,14 +42,34 @@ void q_short(int low, int high)
 {
     int temp;
     int pivot, i, j;
-    if (low > high)
+    if (low > high)  //step 1
     {
         return;
     }
 
-    pivot = arr[low];
-    i = low + 1;
-    j = high;
+    pivot = arr[low];  //step 2
+    i = low + 1;  //step 3
+    j = high;  //step 4
+
+    while (i <= j)  //step 10
+    {
+        while ((arr[i] <= pivot) && (i <= high))  //step 5
+        {
+            i++; //step 6
+            cmp_count++;
+        }
+        cmp_count++;
+        while ((arr[j] > pivot) && (j >= low)) //step 7
+        {
+            j--;  //step 8
+            cmp_count++;
+        }
+        cmp_count++;
+        if (i < j)  //step 9
+        {
+            swap(i, j);
+        }
+    }
 
 }
 
