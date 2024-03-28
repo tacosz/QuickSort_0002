8b76919abded09ffa63a1f1be6a63df1d8afe195
tacosz <nabilnasruddin.offc@gmail.com>
2024-03-28 18:53:17 +0700

    penambahan pemasukan panjang elemen array

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -6,6 +6,20 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang Elemen Array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum Panjang Array adalah 20" << endl;
+    }
+}
+
 int main()
 {
     
